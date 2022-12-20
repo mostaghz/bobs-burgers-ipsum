088fc9cba92eb45abcaeb2ff96b14b1decd0771a
Alex Mostaghni <113554195+mostaghz@users.noreply.github.com>
2022-12-19 22:06:02 -0700

    removed ranger slider for parapgraph selection, added ipsum from S1 E1, and created md file for notes

---
+++ documentation/plans-and-pseudo-code.md
@@ -0,0 +1,12 @@
+### Road Blocks
+
+- Current code along uses set paragraphs of ipsum. So each paragraph generated is the exact same as the time before... I want random sentences or chunks of sentences generated to make a unique paragraph each time. Stick with example to get things working for now.
+
+### Immediate Plan
+
+- Instead of each object being a preset paragraph, have hundreds of objects joined together to make a paragraph... does this require making an api?
+- Look at javascript challenge?
+
+### Future Plans
+
+- Option to choose ipsum based on character and/or episode?
---
+++ script.js
@@ -0,0 +1,7 @@
+const text = [
+    // S1 E1
+    'My crotch is itchy. Okay...are you telling me as my daughter or as my grill cook? Um...as... Because my grill cook would never tell me that.',
+    `I'm just saying it's easy to remember; our anniversary. It's September 3rd. 9/3. Nine is divisible by three. What? That's not...well, a lot of numbers are divisible by three.`,
+    `Hmm. He looks like a child molester. Louise don't serve him; let Gene bring it. Why do I have to get molested? Because he's not gonna molest you. Why? Because you're heavy. Heavy kids can get molested.`,
+    `We're investigating a very disturbing rumor. There's a rumor you make your burgers with human remains from the crematorium next door! Hmm, I wonder how a rumor like that get started... It's made with human remains from the crematorium next door.`
+];
