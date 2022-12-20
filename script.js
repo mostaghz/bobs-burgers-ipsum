0a241a35e3aff5726e64bcf049f9ced6b349470f
Alex Mostaghni <113554195+mostaghz@users.noreply.github.com>
2022-12-19 22:28:54 -0700

    functional ipsum based on example. its all very basic but a good start

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ const text = [
     `Hmm. He looks like a child molester. Louise don't serve him; let Gene bring it. Why do I have to get molested? Because he's not gonna molest you. Why? Because you're heavy. Heavy kids can get molested.`,
     `We're investigating a very disturbing rumor. There's a rumor you make your burgers with human remains from the crematorium next door! Hmm, I wonder how a rumor like that get started... It's made with human remains from the crematorium next door.`
 ];
+
+let form = document.querySelector(".ipsumForm");
+let numOfParas = document.getElementById("numOfParas");
+let result = document.querySelector(".ipsumText");
+
+function syncParaNumber(e) {
+    let value = e.target.value;
+    numOfParas.value = value;
+}
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    let value = parseInt(numOfParas.value, 5);
+    let tempText = text.slice(0, value);
+    tempText = tempText.map(item => `<p class="result">${item}</p>`).join("");
+    result.innerHTML = tempText;
+});
+
+numOfParas.addEventListener('input', syncParaNumber);
